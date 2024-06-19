ca3297aa728705e84d6e5bc2d4fe86fd6fbb6679
BENSFIA <mobensfia@gmail.com>
2024-06-19 13:39:27 +0200

    props.1

--- pockemon/src/App.jsx
+++ pockemon/src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
-import PockemonCard from "./components/PockemonCard";
+import PokemonCard from "./components/PokemonCard";
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 export default function App() {
   return (
     <div>
-      <PockemonCard />
+      <PokemonCard pokemon={pokemonList[0]} />
     </div>
   );
 }
--- pockemon/src/components/PokemonCard.jsx
+++ pockemon/src/components/PokemonCard.jsx
@@ -1,15 +1,4 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-export default function PokemonCard() {
-  const pokemon = pokemonList[0];
+export default function PokemonCard({ pokemon }) {
   return (
     <>
       <div className="pokemon-card">
@@ -17,7 +6,7 @@ export default function PokemonCard() {
         {pokemon.imgSrc ? (
           <img src={pokemon.imgSrc} alt={pokemon.name} />
         ) : (
-          <p>??</p>
+          <p>???</p>
         )}
       </div>
     </>
