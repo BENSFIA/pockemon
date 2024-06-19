368d8a2863c8962c2561b45af0c111d538793e4f
BENSFIA <mobensfia@gmail.com>
2024-06-19 09:50:00 +0200

    compnents.1

---
+++ pockemon/src/components/PockemonCard.jsx
@@ -0,0 +1,13 @@
+export default function PockemonCard() {
+  return (
+    <>
+      <figure>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt=""
+        />
+      </figure>
+      <figcaption>Bulbasaur</figcaption>
+    </>
+  );
+}
